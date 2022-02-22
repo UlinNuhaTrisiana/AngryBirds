42a8df6d08eb24a2519e7ab58c24bdfcf210b999
UlinNuhaTrisiana <nuha7817@gmail.com>
2022-02-22 13:10:48 +0700

    main

---
+++ Assets/Scripts/SlingShooter.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShooter : MonoBehaviour
+{
+    public CircleCollider2D Collider;
+    public LineRenderer Trajectory;
+    private Vector2 _startPos;
+
+    [SerializeField]
+    private float _radius = 0.75f;
+
+    [SerializeField]
+    private float _throwSpeed = 30f;
+
+
+    private Bird _bird;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPos = transform.position;
+    }
+
+    void OnMouseUp() 
+    {
+        Collider.enabled = false;
+        Vector2 velocity = _startPos - (Vector2)transform.position;
+        float distance = Vector2.Distance(_startPos, transform.position);
+
+        _bird.Shoot(velocity, distance, _throwSpeed);
+        
+        // Kembalikan ketapel ke posisi awal
+        gameObject.transform.position = _startPos;
+        Trajectory.enabled = false;
+    }
+
+    void OnMouseDrag() 
+    {
+        // Ubah posisi mouse ke world position
+        Vector2 p = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Hitung agar karet ketapel berada dalam radius yang ditentukan 
+        Vector2 dir = p-_startPos;
+        if (dir.sqrMagnitude > _radius)
+        //{
+            dir = dir.normalized * _radius;
+        //}
+        transform.position = _startPos + dir;
+
+        float distance = Vector2.Distance(_startPos, transform.position);
+
+        if (!Trajectory.enabled)
+        {
+            Trajectory.enabled = true;
+        }
+
+        DisplayTrajectory(distance);
+    }
+
+    // Prediksi posisi burung dg rumus kemudian digambarkan dg menggunakan LineRenderer
+    void DisplayTrajectory(float distance){
+        if (_bird == null)
+        {
+            return;
+        }
+
+        Vector2 velocity = _startPos - (Vector2)transform.position;
+        int segmentCount = 5; // Total titik atau point yg akan digambarkan
+        Vector2[] segments = new Vector2[segmentCount];
+
+        // Posisi awal trajectory merupakan posisi mouse dari player
+        segments[0] = transform.position;
+
+        // Velocity awal
+        Vector2 segVelocity = velocity * _throwSpeed * distance;
+
+        for (int i = 0; i < segmentCount; i++)
+        {
+            float elapsedTime = i * Time.fixedDeltaTime * 5;
+            segments[i] = segments[0] + segVelocity * elapsedTime + 0.5f * Physics2D.gravity * Mathf.Pow(elapsedTime, 2);
+        }
+
+        Trajectory.positionCount = segmentCount;
+        for (int i = 0; i < segmentCount; i++)
+        {
+            Trajectory.SetPosition(i, segments[i]);
+        }
+    }
+
+    public void InitiateBird(Bird bird){
+        _bird = bird;
+        _bird.MoveTo(gameObject.transform.position, gameObject);
+        Collider.enabled = true;
+    }
+
+    
+}
---
+++ Assets/Scripts/TrailController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailController : MonoBehaviour
+{
+    public GameObject Trail; // Menampung prefab GameObject dg komponen sprite
+    public Bird TargetBird; // Burung yang akan diberi trails
+
+    private List<GameObject> _trails;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Inisiasi atribut _trails
+        _trails = new List<GameObject>();
+    }
+
+    // Menambah burung yg akan dijadikan target, dan reset ulang trail
+    public void SetBird(Bird bird){
+        TargetBird = bird;
+
+        for (int i = 0; i < _trails.Count; i++)
+        {
+            Destroy(_trails[i].gameObject);
+        }
+
+        _trails.Clear();
+    }
+
+    // Membuat GameObject trail tiap 100ms
+    public IEnumerator SpawnTrail(){
+        _trails.Add(Instantiate(Trail, TargetBird.transform.position, Quaternion.identity));
+
+        yield return new WaitForSeconds(0.1f);
+
+        if (TargetBird != null && TargetBird.State != Bird.BirdState.HitSomething)
+        {
+            StartCoroutine(SpawnTrail());
+        }
+    }
+}
